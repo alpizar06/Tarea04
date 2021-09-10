f471db8dbf55b2a25669490a26bbe1284614de0d
Daniel Alpizar <alpizar0610@gmail.com>
2021-09-10 01:17:38 -0600

    Add files via upload

---
+++ CalculatorUI/CalculatorUI/CalculatorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorUIApp.swift
+//  CalculatorUI
+//
+//  Created by Daniel Alpizar on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
